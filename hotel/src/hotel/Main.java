3db4b3b38e3b5d9710fe19d865454efcb5e7fd25
DanaeMaria <maraki12vlassi@gmail.com>
2016-01-29 15:08:45 +0200

    hotel

---
+++ hotel/src/hotel/Main.java
@@ -0,0 +1,28 @@
+package hotel;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Main {
+
+ 
+    public static void main(String[] args) {
+
+
+        Main main=new Main();
+
+       NewJFrame mainFrame = new NewJFrame();
+       mainFrame.setVisible(true);
+       
+    }
+
+  
+}
